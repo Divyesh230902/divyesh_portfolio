a11033f139f59f9ac65d4e9fe00266f3c7aa4b74
Divyesh Patel <86349765+Divyesh230902@users.noreply.github.com>
2023-10-24 14:08:51 +0000

    home changed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -26,7 +26,7 @@ const imageAltText = "AI Avatar of Divyesh Patel";
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
-      <img className="background" src="../images/avaturn_screenshot.jpg" alt="" />
+      <img className="background" src={image} alt="" />
       <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
         {/* <h1>{name}</h1>
         <h2>{title}</h2> */}
