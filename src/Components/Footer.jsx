c2a95e62924ae0b7a3ef18f2472a235b75e47f0e
Divyesh Patel <86349765+Divyesh230902@users.noreply.github.com>
2023-10-24 13:19:37 +0000

    footer set

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,15 +10,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import devDotToIcon from "../images/socials/devdotto.svg";
+// import devDotToIcon from "../images/socials/devdotto.svg";
 import envelopeIcon from "../images/socials/envelope.svg";
 import gitHubIcon from "../images/socials/github.svg";
 import instagramIcon from "../images/socials/instagram.svg";
 import linkedInIcon from "../images/socials/linkedin.svg";
-import mediumIcon from "../images/socials/medium.svg";
+// import mediumIcon from "../images/socials/medium.svg";
 import twitterIcon from "../images/socials/twitter.svg";
-import youTubeIcon from "../images/socials/youtube.svg";
-import { getMaxListeners } from "process";
+// import youTubeIcon from "../images/socials/youtube.svg";
+// import { getMaxListeners } from "process";
 
 /**
  * 💡 Learning resources
