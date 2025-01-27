8ec66b8edbd750a25d8d82ce3ddd1cb241c734ff
Divyesh Patel <86349765+Divyesh230902@users.noreply.github.com>
2025-01-27 18:32:37 +0000

    portfolio changed

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Want to Enhance your pixelated images try out my PixEnchar",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This tool uses Genarative AI to Upscale your images. It uses the power of AI to enhance your images.",
+    url: "https://github.com/Divyesh230902/PixEnchar",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Want to have some Awesome AI project Ideas",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Contributed sketch note imagery to accompany each lesson.",
+    url: "https://github.com/Divyesh230902/awesome-generative-ai-guide",
   },
   {
-    title: "My Resume Site",
+    title: "My Resume",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Check out my Resume, to know more about me",
+    url: "https://drive.google.com/file/d/11Z7q71oWoFyUxhYaHJS4p9sDNjUP0HgW/view?usp=share_link",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "My podcast on AI",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Video interview to explain my experience throughout my journey in AI.",
+    url: "https://www.youtube.com/watch?v=npSQ9Pfx_aM",
   },
 ];
 
